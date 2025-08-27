93ba3b9ea01758905763d48633dcf0cec796ced9
Shak Sadi Rimon <shaksadi.rimon@gmail.com>
2025-08-28 04:36:04 +0600

    coin done

--- java.js
+++ java.js
@@ -1,33 +1,53 @@
-function heartButton(id){
-    const heartValue = parseInt(document.getElementById("heart-value").innerText);
+// heart button
+function heartButton(id) {
+  const heartValue = parseInt(document.getElementById("heart-value").innerText);
   const plusValue = 1;
   document.getElementById("heart-value").innerText = heartValue + plusValue;
 }
 
+// heart button increase
 document.getElementById("1heart-btn").addEventListener("click", function () {
-  heartButton("1heart-btn")
+  heartButton("1heart-btn");
 });
 document.getElementById("2heart-btn").addEventListener("click", function () {
-  heartButton("2heart-btn")
+  heartButton("2heart-btn");
 });
 document.getElementById("3heart-btn").addEventListener("click", function () {
-  heartButton("3heart-btn")
+  heartButton("3heart-btn");
 });
 document.getElementById("4heart-btn").addEventListener("click", function () {
-  heartButton("4heart-btn")
+  heartButton("4heart-btn");
 });
 document.getElementById("5heart-btn").addEventListener("click", function () {
-  heartButton("5heart-btn")
+  heartButton("5heart-btn");
 });
 document.getElementById("6heart-btn").addEventListener("click", function () {
-  heartButton("6heart-btn")
+  heartButton("6heart-btn");
 });
 document.getElementById("7heart-btn").addEventListener("click", function () {
-  heartButton("7heart-btn")
+  heartButton("7heart-btn");
 });
 document.getElementById("8heart-btn").addEventListener("click", function () {
-  heartButton("8heart-btn")
+  heartButton("8heart-btn");
 });
 document.getElementById("9heart-btn").addEventListener("click", function () {
-  heartButton("9heart-btn")
+  heartButton("9heart-btn");
+});
+
+document.querySelectorAll(".call-btn").forEach((button) => {
+  button.addEventListener("click", function () {
+    const card = this.closest(".card");
+    const name = card.querySelector(".service-name").innerText;
+    const number = card.querySelector(".number").innerText;
+    let coinValue = parseInt(document.getElementById("coin-value").innerText);
+    console.log(coinValue);
+    if (coinValue >= 20) {
+      document.getElementById("coin-value").innerText = coinValue - 20;
+      alert(`📞 Calling ${name} ${number}`);
+    }
+    else{
+        alert("❌ Not Enough Coins ❌")
+    }
+    
+  });
 });
